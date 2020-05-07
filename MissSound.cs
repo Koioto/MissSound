cd39a0ed0bad8241cbc199a1cf19c8f163e193fe
AioiLight <info@aioilight.space>
2020-05-07 20:11:29 +0900

    サンプルプラグイン用

---
+++ MissSound.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Amaoto;
+using Koioto.Plugin;
+using Koioto.Support;
+using Koioto.Support.FileReader;
+
+namespace Koioto.SamplePlugin.MissSound
+{
+    public class MissSound : Overlay
+    {
+        public override string Name => "MissSound";
+        public override string[] Creator => new string[] { "AioiLight" };
+        public override string Description => "Play sound when miss.";
+
+        public override void OnEnable()
+        {
+            // プラグインのフォルダとプラグイン名を結合してプラグインが使用するフォルダを決定する
+            // Combine the plugin's folder with the plugin name to determine which folder the plugin will use
+            var dir = Path.Combine(Bridge.PluginDir, Name);
+            // Amaoto.Soundのインスタンスを作成して音を鳴らす準備をする
+            // Create an instance of Amaoto.Sound and get ready to play the sound.
+            Sound = new Sound(Path.Combine(dir, @"miss-sound.wav"));
+            base.OnEnable();
+        }
+
+        public override void OnDisable()
+        {
+            // 音の破棄をする。忘れるとメモリリークする可能性があるので必ずすること
+            // Dipose the sound.
+            // Be sure to do this because there is a possibility of a memory leak if you forget to do it.
+            Sound?.Dispose();
+            base.OnDisable();
+        }
+
+        public override void OnHitNote(Chip chip, int player)
+        {
+            // チップがヒットしたときの処理
+            // What to do when a chip is hit
+            if (chip.IsNote && chip.IsHitted)
+            {
+                // 音符である、かつチップがヒットしたとき
+                // When it is a note and the chip is hit
+                if (chip.Judge == Judge.Miss || chip.Judge == Judge.Bad)
+                {
+                    // 判定が見過ごし不可、または叩いて不可のとき、音を鳴らす。
+                    // A sound is sounded when the judgement is Miss or Bad.
+                    Sound?.Play();
+                }
+            }
+            base.OnHitNote(chip, player);
+        }
+
+        private Sound Sound;
+    }
+}
